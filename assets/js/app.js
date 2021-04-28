57eef25b2a44cf0468231ef2e7f4fd5a75eecf12
Sam Ziegler <szig97@outlook.com>
2021-04-28 12:29:47 -0500

    creating svg wrapper

--- assets/js/app.js
+++ assets/js/app.js
@@ -10,3 +10,16 @@ var margin = {
 
 var width = svgWidth - margin.left - margin.right;
 var height = svgHeight - margin.top - margin.bottom;
+
+// Create SVG wrapper
+var svg = d3.select("body")
+    .append("svg")
+    .attr("width", svgWidth)
+    .attr("height", svgHeight);
+
+var chartGroup = svg.append("g")
+    .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+d3.select("body").append("div").attr("class", "tooltip").style("opacity", 0);
+
+
